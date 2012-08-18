01b72637c0b2deebb0e86c64ace0f413d3de055f
CoryNelson <Cory-Nelson@hotmail.co.uk>
2012-08-18 02:32:39 +0100

    Update

---
+++ src/scripts/SpiceLooter/paint/Paint.java
@@ -0,0 +1,9 @@
+package scripts.SpiceLooter.paint;
+
+/**
+ * User: Cory
+ * Date: 18/08/12
+ * Time: 02:31
+ */
+public class Paint {
+}
---
+++ src/scripts/SpiceLooter/tasks/ChaseVermin.java
@@ -0,0 +1,18 @@
+package scripts.SpiceLooter.tasks;/**
+ * User: Cory
+ * Date: 18/08/12
+ * Time: 02:31
+ */
+
+import org.powerbot.concurrent.Task;
+import org.powerbot.concurrent.strategy.Strategy;
+
+public class ChaseVermin extends Strategy implements Task {
+	public boolean validate() {
+		return false;
+	}
+
+	public void run() {
+
+	}
+}
