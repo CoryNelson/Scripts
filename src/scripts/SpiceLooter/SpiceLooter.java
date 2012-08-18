a9d96456e66a7aac7aba4b959a32d2d2ebd8f22d
CoryNelson <Cory-Nelson@hotmail.co.uk>
2012-08-18 02:17:02 +0100

    Update

---
+++ src/scripts/SpiceLooter/SpiceLooter.java
@@ -0,0 +1,23 @@
+package scripts.SpiceLooter;
+
+import org.powerbot.game.api.ActiveScript;
+import org.powerbot.game.bot.event.listener.PaintListener;
+
+import java.awt.*;
+
+/**
+ * User: Cory
+ * Date: 18/08/12
+ * Time: 02:11
+ */
+public class SpiceLooter extends ActiveScript implements PaintListener {
+
+	public void setup() {
+
+	}
+
+	public void onRepaint(Graphics g) {
+
+	}
+
+}
