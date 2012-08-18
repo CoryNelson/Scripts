69186868fddda1c25e209288aebfbf1a18bf7558
CoryNelson <Cory-Nelson@hotmail.co.uk>
2012-08-18 02:38:40 +0100

    Update

--- src/scripts/SpiceLooter/paint/Paint.java
+++ src/scripts/SpiceLooter/paint/Paint.java
@@ -1,9 +1,14 @@
 package scripts.SpiceLooter.paint;
 
+import java.awt.*;
+
 /**
- * User: Cory
- * Date: 18/08/12
- * Time: 02:31
- */
+* User: Cory
+	* Date: 18/08/12
+	* Time: 02:31
+	*/
 public class Paint {
+	public static void draw(Graphics g) {
+
+	}
 }
