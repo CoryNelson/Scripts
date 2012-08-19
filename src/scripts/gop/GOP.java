a293612bd494af24e5917bb0d4cafd98787d34cb
CoryNelson <spudr0x@hotmail.co.uk>
2012-08-19 01:42:51 +0100

    Update

---
+++ src/scripts/gop/GOP.java
@@ -0,0 +1,17 @@
+package scripts.gop;
+
+import org.powerbot.game.api.ActiveScript;
+import org.powerbot.game.bot.event.listener.PaintListener;
+
+import java.awt.*;
+
+public class GOP extends ActiveScript implements PaintListener {
+
+	protected void setup() {
+
+	}
+
+	public void onRepaint(Graphics graphics) {
+
+	}
+}
